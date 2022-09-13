04771b9fc4c412d412dacf3e4979cdb8f184ad6b
remarshal <marshalwaldendev@gmail.com>
2022-09-13 15:51:30 -0400

    best practice setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'Brandon'
-    }
+      name: "Brandon",
+    };
   }
-  render() {    
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name}
-          </p>
-          <button onClick={() => {
-            this.setState({name: 'Marshal'})
-          }}>Change Name</button>
+          <p>Hi {this.state.name}</p>
+          <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  return {
+                    name: "Marshal",
+                  };
+                },
+                () => {
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change Name
+          </button>
         </header>
       </div>
     );
