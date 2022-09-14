b04c2624909f266a10a34eed4cb6112b6f369d01
remarshal <marshalwaldendev@gmail.com>
2022-09-14 10:48:27 -0400

    componentDidMount json placeholders

--- src/App.js
+++ src/App.js
@@ -10,6 +10,19 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(
+        () => {
+          return {structures: users}
+        },
+        () => {
+          console.log(this.state)
+        }
+    ))
+  }
+
   render() {
     return (
       <div className="App">
