af3f11168be15d026b0b55b7d8c9acf62c79a1c3
remarshal <marshalwaldendev@gmail.com>
2022-09-14 16:25:48 -0400

    search box

--- src/App.js
+++ src/App.js
@@ -6,32 +6,47 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      structures: [ ],
+      structures: [],
     };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState(
-        () => {
-          return {structures: users}
-        },
-        () => {
-          console.log(this.state)
-        }
-    ))
+      .then((users) =>
+        this.setState(
+          () => {
+            return { structures: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
         <h1>Provincetown Structures</h1>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search addresses"
+          onChange={(event) => {
+            const filteredStructures = this.state.structures.filter((address) => {
+              return address.name.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase());
+            });
+            this.setState(() => {
+              return{structures: filteredStructures}
+            })
+          }}
+        />
         {this.state.structures.map((structure) => {
           return (
             <div key={structure.id}>
               <h2>{structure.name}</h2>
-              <p>{structure.nickname}</p>
+              <p>{structure.address.street}</p>
             </div>
           );
         })}
