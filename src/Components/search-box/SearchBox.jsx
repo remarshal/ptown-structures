3d5670d5284689dfde95cac093dba2e311c7aad1
remarshal <marshalwaldendev@gmail.com>
2022-09-26 14:52:22 -0400

    setup initial components

---
+++ src/Components/search-box/SearchBox.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class SearchBox extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default SearchBox;
