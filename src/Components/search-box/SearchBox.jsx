a6e2e2d7a093ab86b40288f96d9ffb76e333192a
remarshal <marshalwaldendev@gmail.com>
2022-10-04 15:20:31 -0400

    interpolate className

--- src/Components/search-box/SearchBox.jsx
+++ src/Components/search-box/SearchBox.jsx
@@ -5,7 +5,7 @@ class SearchBox extends Component {
         return (
             <div>
                 <input
-                    className={this.props.className}
+                    className={`search-box ${this.props.className}`}
                     type="search"
                     placeholder={this.props.placeholder}
                     onChange={this.props.onChangeHandler}
