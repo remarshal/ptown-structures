c8471484010340a7d4513f7501ba0d4393a8f9b8
remarshal <marshalwaldendev@gmail.com>
2022-10-04 16:04:45 -0400

    added images

--- src/Components/card-list/CardList.jsx
+++ src/Components/card-list/CardList.jsx
@@ -7,6 +7,7 @@ class CardList extends Component {
                 {this.props.structures.map((structure) => {
                     return (
                         <div key={structure.id}>
+                            <img alt={`structure ${structure.name}`} src={`https://robohash.org/${structure.id}?set=set4&size=180x180`} />
                             <h2>{structure.name}</h2>
                             <p>{structure.address.street}</p>
                         </div>
