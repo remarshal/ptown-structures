a897d952fb2d39a197fe7b7b2acc42e37d6cc3a6
remarshal <marshalwaldendev@gmail.com>
2022-10-12 14:19:30 -0400

    missed a bracket

--- src/scrimba/scrimba-lesson.js
+++ src/scrimba/scrimba-lesson.js
@@ -17,15 +17,16 @@ export default class App extends React.Component {
         phone: "+1 (719) 555-1212",
         email: "itsmyrealname@example.com",
         isFavorite: false
-    }
+    };
 
     toggleFavorite = () => {
-        this.setState(prevContact => {
+        this.setState(prevContact => ({
             isFavorite: !prevContact.isFavorite
-        })
+        }));
+    }
 
-        render() 
-            let starIcon = this.state.isFavorite ? "star-filled.png" : "star-empty.png"
+        render() {
+            let starIcon = this.state.isFavorite ? "star-filled.png" : "star-empty.png";
             return (
                 <main>
                     <article className="card">
@@ -37,14 +38,14 @@ export default class App extends React.Component {
                                 onClick={this.toggleFavorite}
                             />
                             <h2 className="card--name">
-                                {this.state.contact.firstName} {this.state.contact.lastName}
+                                {this.state.firstName} {this.state.lastName}
                             </h2>
-                            <p className="card--contact">{this.state.contact.phone}</p>
-                            <p className="card--contact">{this.state.contact.email}</p>
+                            <p className="card--contact">{this.state.phone}</p>
+                            <p className="card--contact">{this.state.email}</p>
                         </div>
 
                     </article>
                 </main>
-            )
+            );
         }
-    }
+    };
