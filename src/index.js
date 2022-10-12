e87dd6d8ccaefb61f0bd2a0be9e7035e82670cc5
remarshal <marshalwaldendev@gmail.com>
2022-10-12 09:47:25 -0400

    scrimba - functional component setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App-functional';
+import App from './app-scrimba';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
