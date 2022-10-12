e4a0737d0e68ffcb3afa11d8637560e0fe69151d
remarshal <marshalwaldendev@gmail.com>
2022-10-12 14:08:10 -0400

    scrimba lesson

---
+++ src/scrimba/scrimba-lesson.js
@@ -0,0 +1,50 @@
+import React from "react"
+
+/**
+ * Challenge: update the function component to a class component.
+ * 
+ * Bonus: you can use class fields and arrow function methods
+ * to avoid needing to add a constructor method 😄
+ * 
+ * Tip: Move the `let starIcon = ...` line into the `render` method
+ * before the `return`, since that is display logic
+ */
+export default class App extends React.Component {
+
+    state = {
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (719) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    }
+
+    toggleFavorite = () => {
+        this.setState(prevContact => {
+            isFavorite: !prevContact.isFavorite
+        })
+
+        render() 
+            let starIcon = this.state.isFavorite ? "star-filled.png" : "star-empty.png"
+            return (
+                <main>
+                    <article className="card">
+                        <img src="./images/user.png" className="card--image" />
+                        <div className="card--info">
+                            <img
+                                src={`../images/${starIcon}`}
+                                className="card--favorite"
+                                onClick={this.toggleFavorite}
+                            />
+                            <h2 className="card--name">
+                                {this.state.contact.firstName} {this.state.contact.lastName}
+                            </h2>
+                            <p className="card--contact">{this.state.contact.phone}</p>
+                            <p className="card--contact">{this.state.contact.email}</p>
+                        </div>
+
+                    </article>
+                </main>
+            )
+        }
+    }
