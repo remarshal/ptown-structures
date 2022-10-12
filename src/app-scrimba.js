20aa46a6f21e0f37d7467b8ddc4bc6ba995fdbca
remarshal <marshalwaldendev@gmail.com>
2022-10-12 09:59:10 -0400

    convert funct to class

--- src/app-scrimba.js
+++ src/app-scrimba.js
@@ -1,6 +1,5 @@
-
-import React from "react"
-import ReactDOM from "react-dom"
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 /*
 
@@ -11,44 +10,80 @@ Challenge:
 */
 
 // #1
-function App() {
+// function App() {
+//     return (
+//         <div>
+//             <Header username="Marshal"/>
+//             <Greeting />
+//         </div>
+//     )
+// }
+
+class App extends Component {
+  render() {
     return (
-        <div>
-            <Header username="Marshal"/>
-            <Greeting />
-        </div>
-    )
+      <div>
+        <Header username="Marshal" />
+        <Greeting />
+      </div>
+    );
+  }
 }
 
 // #2
-function Header(props) {
+// function Header(props) {
+//     return (
+//         <header>
+//             <p>Welcome, {props.username}!</p>
+//         </header>
+//     )
+// }
+
+class Header extends Component {
+  render() {
     return (
-        <header>
-            <p>Welcome, {props.username}!</p>
-        </header>
-    )
+      <header>
+        <p>Welcome, {this.props.username}!</p>
+      </header>
+    );
+  }
 }
 
 // #3
 // Hint: any "display logic" can be placed inside the `render`
 // method before the `return` statement
-function Greeting() {
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay
-    
+// function Greeting() {
+//   const date = new Date();
+//   const hours = date.getHours();
+//   let timeOfDay;
+
+//   if (hours < 12) {
+//     timeOfDay = "morning";
+//   } else if (hours >= 12 && hours < 17) {
+//     timeOfDay = "afternoon";
+//   } else {
+//     timeOfDay = "night";
+//   }
+//   return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+// }
+
+class Greeting extends Component {
+  render() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+
     if (hours < 12) {
-        timeOfDay = "morning"
+      timeOfDay = "morning";
     } else if (hours >= 12 && hours < 17) {
-        timeOfDay = "afternoon"
+      timeOfDay = "afternoon";
     } else {
-        timeOfDay = "night"
+      timeOfDay = "night";
     }
-    return (
-        <h1>Good {timeOfDay} to you, sir or madam!</h1>
-    )
+    return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
