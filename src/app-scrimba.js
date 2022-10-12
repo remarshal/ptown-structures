f3bbde72bf4c49b629714647048c8b68dd84e5e2
remarshal <marshalwaldendev@gmail.com>
2022-10-12 10:04:47 -0400

    initial funct setup

--- src/app-scrimba.js
+++ src/app-scrimba.js
@@ -1,89 +1,27 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-/*
-
-Challenge:
-1. Convert all 3 components to be class-based
-2. Fix the small bug
-
-*/
-
-// #1
-// function App() {
-//     return (
-//         <div>
-//             <Header username="Marshal"/>
-//             <Greeting />
-//         </div>
-//     )
-// }
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header username="Marshal" />
-        <Greeting />
-      </div>
-    );
-  }
-}
-
-// #2
-// function Header(props) {
-//     return (
-//         <header>
-//             <p>Welcome, {props.username}!</p>
-//         </header>
-//     )
-// }
-
-class Header extends Component {
-  render() {
-    return (
-      <header>
-        <p>Welcome, {this.props.username}!</p>
-      </header>
-    );
-  }
-}
-
-// #3
-// Hint: any "display logic" can be placed inside the `render`
-// method before the `return` statement
-// function Greeting() {
-//   const date = new Date();
-//   const hours = date.getHours();
-//   let timeOfDay;
-
-//   if (hours < 12) {
-//     timeOfDay = "morning";
-//   } else if (hours >= 12 && hours < 17) {
-//     timeOfDay = "afternoon";
-//   } else {
-//     timeOfDay = "night";
-//   }
-//   return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
-// }
-
-class Greeting extends Component {
-  render() {
-    const date = new Date();
-    const hours = date.getHours();
-    let timeOfDay;
-
-    if (hours < 12) {
-      timeOfDay = "morning";
-    } else if (hours >= 12 && hours < 17) {
-      timeOfDay = "afternoon";
-    } else {
-      timeOfDay = "night";
+function App() {
+    const [goOut, setGoOut] = React.useState("Yes")
+    
+    function toggleGoOut() {
+        setGoOut(prevState => {
+            return prevState === "Yes" ? "No" : "Yes"
+        })
     }
-    return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
-  }
+    
+    return (
+        <div className="state">
+            <h1 className="state--title">Should I go out tonight?</h1>
+            <div
+                
+                className="state--value" onClick={toggleGoOut}>
+                <h1>{goOut}</h1>
+            </div>
+        </div>
+    )
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
