896edba0b82025b3e2deb841c1ba7f7c144287a4
remarshal <marshalwaldendev@gmail.com>
2022-10-12 10:09:39 -0400

    added css

--- src/app-scrimba.js
+++ src/app-scrimba.js
@@ -1,25 +1,23 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import "./App.css";
 
 function App() {
-    const [goOut, setGoOut] = React.useState("Yes")
-    
-    function toggleGoOut() {
-        setGoOut(prevState => {
-            return prevState === "Yes" ? "No" : "Yes"
-        })
-    }
-    
-    return (
-        <div className="state">
-            <h1 className="state--title">Should I go out tonight?</h1>
-            <div
-                
-                className="state--value" onClick={toggleGoOut}>
-                <h1>{goOut}</h1>
-            </div>
-        </div>
-    )
+  const [goOut, setGoOut] = React.useState("Yes");
+
+  function toggleGoOut() {
+    setGoOut((prevState) => {
+      return prevState === "Yes" ? "No" : "Yes";
+    });
+  }
+
+  return (
+    <div className="state">
+      <h1 className="state--title">Should I go walk at lunch?</h1>
+      <button className="state--value button" onClick={toggleGoOut}>
+        {goOut}
+      </button>
+    </div>
+  );
 }
 
 // ReactDOM.render(<App />, document.getElementById("root"));
